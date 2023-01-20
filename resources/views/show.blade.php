6627da464c05c0e558a35c8266a543cec950bdaa
nembo93 <francesco.c193@libero.it>
2023-01-20 17:45:58 +0100

    Aggiunge metodo destroy

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,6 +6,11 @@
     </div>
     <h3>{{$comic['series']}}</h3>
     <a href="{{ route('comics.index') }}">Torna ai fumetti</a>
+    <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger">Cancella</button>
+      </form>
 </div>
 @endsection
 
