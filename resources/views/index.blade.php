faeb7a1e56e01c64c7bafc53a12b559aa69f063f
nembo93 <francesco.c193@libero.it>
2023-01-20 11:39:59 +0100

    Estende la index con il file layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,29 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main class="bg-light">
-        <h1>Questa è la view index</h1>
-        @foreach ($comics as $comic)
-            <h2>{{$comic['series']}}</h2>
-        @endforeach
-    </main>
-
-</body>
-
-</html>
+@section('page_content')
+<main class="bg-light">
+    <h1>Questa è la view index</h1>
+    @foreach ($comics as $comic)
+        <h2>{{$comic['series']}}</h2>
+    @endforeach
+</main>
+@endsection
+    
